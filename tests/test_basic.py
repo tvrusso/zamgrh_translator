189dab77ac9f4d2267ca0277e561e4348f07b1ef
Tom Russo <russo@bogodyn.org>
2026-03-30 15:18:28 -0600

    Remove very old test script
    test_basic.py was put in very, very early, and only tested a single
    call to zamgrh_to_english.   zamgrh_to_english has diverged from the
    call prototype that is used here, and this script is no longer
    valuable.   test_translator.py is much more thorough and is used
    constantly in development.

--- tests/test_basic.py
+++
@@ -1,8 +0,0 @@
-from src.translator import zamgrh_to_english, build_lookup, load_dictionary
-
-def test_basic_translation():
-    data = load_dictionary()
-    lookup = build_lookup(data)
-
-    result = zamgrh_to_english("mah ganna nam bra!n", lookup)
-    assert "eat" in result
